eb9962876e264e86cd950f66a9773eafd6e4465f
Bak David <bak.david96@gmail.com>
2022-05-16 17:04:44 +0300

    Grouping test into suite

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,13 +1,15 @@
-import { render, screen } from '@testing-library/react';
-import Greeting from './Greeting';
+import { render, screen } from "@testing-library/react";
+import Greeting from "./Greeting";
 
-test('renders Hello World as a text', () => {
-  // Arrange
-  render(<Greeting />);
+describe("Greeting component", () => {
+  test("renders Hello World as a text", () => {
+    // Arrange
+    render(<Greeting />);
 
-  // Act  here is nothing
+    // Act  here is nothing
 
-  // Assert
-  const linkElement = screen.getByText(/hello world/i);
-  expect(linkElement).toBeInTheDocument();
+    // Assert
+    const linkElement = screen.getByText(/hello world/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
