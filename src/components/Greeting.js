80528dc5cf5099200cf148c67872a19eecd388e0
Bak David <bak.david96@gmail.com>
2022-05-16 23:04:46 +0300

    Writing unit tests for Greeting component

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { useState } from "react";
 
 function Greeting() {
+  const [changedText, setChangedText] = useState(false);
+  const changeTextHanlder = () => {
+    setChangedText(true);
+  };
+
   return (
     <div>
       <h2>Hello World</h2>
-      <p>It's good to see you!</p>
+      {!changedText && <p>It's good to see you!</p>}
+      {changedText && <p>Changed!</p>}
+      <button onClick={changeTextHanlder}>Change text!</button>
     </div>
   );
 }
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
 describe("Greeting component", () => {
@@ -11,5 +12,33 @@ describe("Greeting component", () => {
     // Assert
     const linkElement = screen.getByText(/hello world/i);
     expect(linkElement).toBeInTheDocument();
+  });
+
+  test("renders it's good to see you without button click", () => {
+    render(<Greeting />);
+    const outputElement = screen.getByText("It's good to see you!");
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("renders changed if button was clicked", () => {
+    // Arrange
+    render(<Greeting />);
+    // Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+    // Assert
+    const outputElement = screen.getByText("Changed!");
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("check if it's good to see you is not visible when button clicked", () => {
+    // Arrange
+    render(<Greeting />);
+    // Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+    // Assert
+    const outputElement = screen.queryByText("good to see you!", {exact:false});
+    expect(outputElement).toBeNull();
   });
 });
