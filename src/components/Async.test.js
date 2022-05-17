222d922ecc75808f94c8bc9f1fb72430526c10cc
Bak David <bak.david96@gmail.com>
2022-05-17 14:54:33 +0300

    Using mock to not make the request to the endpoint

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -3,7 +3,11 @@ import Async from "./Async";
 
 describe("Async component", () => {
   test("renders post if request succeeds", async () => {
-    // Arrange
+    // Arrange ( this way we are not making the request to the endpoint)
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: "p1", title: "First post" }],
+    });
     render(<Async />);
 
     // Act  here is nothing
